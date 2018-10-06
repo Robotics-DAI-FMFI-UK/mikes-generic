4ab3a9a0ab7a2723e5e1619ef009ff543cbbf87f
matejka <dusan.matejka@touch4it.com>
2018-10-07 00:35:54 +0200

    Console control config fix

--- console-control/core/config_mikes.c
+++ console-control/core/config_mikes.c
@@ -8,7 +8,7 @@ mikes_config_t mikes_config;
 void load_config()
 {
     config_data cfg = read_config(MIKES_CONFIG);
-    if (cfg == 0) 
+    if (cfg == 0)
     {
        printf("Could not open config file %s\n", MIKES_CONFIG);
        program_runs = 0;
@@ -16,6 +16,8 @@ void load_config()
     mikes_config.print_all_logs_to_console = config_get_intval(cfg, "print_all_logs_to_console", 0);
     mikes_config.print_debug_logs = config_get_intval(cfg, "print_debug_logs", 0);
     mikes_config.use_ncurses_control = config_get_intval(cfg, "use_ncurses_control", 0);
+
+    mikes_config.map_azimuth = config_get_doubleval(cfg, "map_azimuth", mikes_config.map_azimuth);
+
     config_dispose(cfg);
 }
-
